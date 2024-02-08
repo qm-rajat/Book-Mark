00dcf8d31c172e51479b8807647a346f3de213a7
Rajat Kumar Dash <rajatdash2004@outlook.com>
2024-02-09 00:18:30 +0530

    The book mark
    My 1st project .
    This online book store provide you book for free

---
+++ Product_det_28.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Details</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="./resource/logo (2).png" alt="" width="200px">
+            </div>
+
+            <nav>
+            <ul id="MenuItems">
+                    <li><a href="main.php">HOME</a></li>
+                    <li><a href="All_Products.php">PRODUCT</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                  </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="small-container single-product">
+        <div class="row">
+            <div class="col-2">
+                <img src="resource\aa\Android Hacker's Handbook.png" alt="" width="100%" id="product-img">
+            </div>
+
+            <div class="col-2">
+                <p>Home/Book</p>
+                <h1>Android Hacker's Handbook.png</h1>
+             
+               
+                <a href="resource\Book\Android Hacker's Handbook by Joshua J. Drake, Zach Lanier, Collin Mulliner, Pau Oliva Fora, Stephen A. Ridley and Georg Wicherski.pdf" download="Book" class="btn">Download</a>
+                <h3>Product Details  <i class="fa fa-indent"></i></h3>
+                <br>
+                <p>The "Android Hacker's Handbook" is an in-depth guide for security professionals and ethical hackers interested in understanding and securing the Android operating system. Authored by various experts in the field, the book covers a wide range of topics related to Android security, including app security, network security, and device security. It provides insights into the vulnerabilities and attack vectors that can be exploited by hackers and offers guidance on securing Android applications and devices. With practical examples and real-world case studies, the handbook is a valuable resource for those involved in penetration testing and securing Android environments against potential threats.</p>
+            </div>
+        </div>
+    </div>
+
+   
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Downlaod App for Android and ios mobile phone.</p>
+
+                    <div class="app-logo">
+                        <img src="./resource/play-store.png" alt="">
+                        <img src="./resource/app-store.png" alt="">
+                    </div>
+                </div>
+
+                <div class="footer-col-2">
+                    <img src="./resource/logo (2).png" alt="">
+                    <p>Our Purpose Is To Sustainably Make the Pleasure and Benifits of Sports Accessible to the many</p>
+                </div>
+
+                <div class="footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>Join Affilate</li>
+                    </ul>
+                </div>
+
+                <div class="footer-col-4">
+                    <h3>Folllow Us</h3>
+                    <ul>
+                        <li>FaceBook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+
+            <hr>
+            <p class="copyright">Copyright 2023 - Rajat Creations</p>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ Product_det_36.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Details</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="./resource/logo (2).png" alt="" width="200px">
+            </div>
+
+            <nav>
+            <ul id="MenuItems">
+                    <li><a href="main.php">HOME</a></li>
+                    <li><a href="All_Products.php">PRODUCT</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                    </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div class="small-container single-product">
+        <div class="row">
+            <div class="col-2">
+                <img src="resource\aa\Carnivore Code Cookbook_ Reclaim Your Health, Strength, and Vitality With 100_ Delicious Recipes, The - Paul Saladino.png" alt="" width="100%" id="product-img">
+            </div>
+
+            <div class="col-2">
+                <p>Home/Book</p>
+                <h1>Carnivore Code Cookbook</h1>
+             
+               
+                <a href="resource\Book\Carnivore Code Cookbook_ Reclaim Your Health, Strength, and Vitality With 100_ Delicious Recipes, The - Paul Saladino.pdf" download="Book" class="btn">Download</a>
+                <h3>Product Details  <i class="fa fa-indent"></i></h3>
+                <br>
+                <p>As of my last update in January 2022, I don't have specific information about a cookbook titled "Carnivore Code Cookbook." It's advisable to check recent publications and the author's official channels for the latest information. If it's related to a specific diet or lifestyle, it might provide recipes aligning with the principles of the carnivore diet. For accurate details and availability, please refer to recent sources or the author's official platforms.</p>
+            </div>
+        </div>
+    </div>
+
+   
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Downlaod App for Android and ios mobile phone.</p>
+
+                    <div class="app-logo">
+                        <img src="./resource/play-store.png" alt="">
+                        <img src="./resource/app-store.png" alt="">
+                    </div>
+                </div>
+
+                <div class="footer-col-2">
+                    <img src="./resource/logo (2).png" alt="">
+                    <p>Our Purpose Is To Sustainably Make the Pleasure and Benifits of Sports Accessible to the many</p>
+                </div>
+
+                <div class="footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>Join Affilate</li>
+                    </ul>
+                </div>
+
+                <div class="footer-col-4">
+                    <h3>Folllow Us</h3>
+                    <ul>
+                        <li>FaceBook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+
+            <hr>
+            <p class="copyright">Copyright 2023 - Rajat Creations</p>
+        </div>
+    </div>
+
+
+</body>
+</html>
